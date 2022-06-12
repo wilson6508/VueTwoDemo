ee795306542da51ce28a3e90724e1eb4d5af1673
wilson6508 <wilson650837@gmail.com>
2022-06-12 20:21:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Navigation />
+  </div>
+</template>
+
+<script>
+import Navigation from "@/components/Navigation.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navigation,
+  },
+};
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="d-flex justify-content-start">
+        <div class="mt-2 mb-1 text-light">
+          <a class="ms-3 navbar-brand" style="cursor: pointer">首頁</a>
+          <a class="ms-3 navbar-brand" style="cursor: pointer">美國股市</a>
+          <a class="ms-3 navbar-brand" style="cursor: pointer">運動賽事</a>
+          <a class="ms-3 navbar-brand" style="cursor: pointer">Test01</a>
+          <a class="ms-3 navbar-brand" style="cursor: pointer">Test02</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data() {
+    return {
+      test: 1234,
+    };
+  },
+};
+</script>
