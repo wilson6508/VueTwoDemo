82632d4c19e3988e2959995d5d8c875402f20e14
wilson6508 <wilson650837@gmail.com>
2022-11-13 09:26:42 +0800

    Update Actuator

--- src/components/springboot/Actuator.vue
+++ src/components/springboot/Actuator.vue
@@ -35,7 +35,7 @@ management:
     shutdown:
       enabled: true
         </pre
-            >
+        >
           </template>
         </CodeContainer>
       </div>
@@ -43,18 +43,18 @@ management:
         <CodeContainer :title="'application.properties'">
           <template v-slot:sourceCode>
             <pre>
-management.endpoints.web.exposure.include=*                
+management.endpoints.web.exposure.include=*
 management.endpoint.shutdown.enabled=true
         </pre
-            >
+        >
           </template>
         </CodeContainer>
       </div>
       <div class="col-md-8 col-12">
-        <CodeContainer :title="'Postman'">
+        <CodeContainer :title="'postman'">
           <template v-slot:sourceCode>
             <pre>
-Get                
+Get
 http://127.0.0.1:8086/actuator/env
 http://127.0.0.1:8086/actuator/health
 http://127.0.0.1:8086/actuator/scheduledtasks
@@ -63,6 +63,90 @@ http://127.0.0.1:8086/actuator/metrics/system.cpu.usage
 Post
 http://127.0.0.1:8086/actuator/shutdown
         </pre>
+          </template>
+        </CodeContainer>
+      </div>
+      <hr />
+      <div class="col-md-8 col-12">
+        <CodeContainer :title="'AdminApi'" :language="'html'">
+          <template v-slot:sourceCode>
+            <pre>
+&lt;dependency&gt;
+    &lt;groupId&gt;de.codecentric&lt;/groupId&gt;
+    &lt;artifactId&gt;spring-boot-admin-starter-server&lt;/artifactId&gt;
+    &lt;version&gt;2.6.1&lt;/version&gt;
+&lt;/dependency&gt;
+            </pre>
+          </template>
+        </CodeContainer>
+      </div>
+      <div class="col-md-8 col-12">
+        <CodeContainer :title="''">
+          <template v-slot:sourceCode>
+            <pre>
+server.port=10001
+            </pre>
+          </template>
+        </CodeContainer>
+      </div>
+      <div class="col-md-8 col-12">
+        <CodeContainer :title="''" :language="'js'">
+          <template v-slot:sourceCode>
+            <pre>
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+            </pre>
+          </template>
+        </CodeContainer>
+      </div>
+      <hr />
+      <div class="col-md-8 col-12">
+        <CodeContainer :title="'WebServerApi'" :language="'html'">
+          <template v-slot:sourceCode>
+            <pre>
+&lt;dependency&gt;
+    &lt;groupId&gt;de.codecentric&lt;/groupId&gt;
+    &lt;artifactId&gt;spring-boot-admin-starter-client&lt;/artifactId&gt;
+    &lt;version&gt;2.6.1&lt;/version&gt;
+&lt;/dependency&gt;
+&lt;dependency&gt;
+    &lt;groupId&gt;org.springframework.boot&lt;/groupId&gt;
+    &lt;artifactId&gt;spring-boot-starter-actuator&lt;/artifactId&gt;
+&lt;/dependency&gt;
+            </pre>
+          </template>
+        </CodeContainer>
+      </div>
+      <div class="col-md-8 col-12">
+        <CodeContainer :title="''">
+          <template v-slot:sourceCode>
+            <pre>
+server:
+  port: 10002
+spring:
+  application:
+    name: WebServerApi
+  boot:
+    admin:
+      client:
+        url: http://127.0.0.1:10001
+management:
+  endpoints:
+    web:
+      exposure:
+        include: '*'
+            </pre>
           </template>
         </CodeContainer>
       </div>
