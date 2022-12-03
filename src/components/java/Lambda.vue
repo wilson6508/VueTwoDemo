10c1ec713fd43a4221d10e4538f0296aae1bd258
wilson6508 <wilson650837@gmail.com>
2022-12-03 22:37:23 +0800

    Update Lambda

--- src/components/java/Lambda.vue
+++ src/components/java/Lambda.vue
@@ -1,73 +1,36 @@
 <template>
-  <div>
-    <div class="row m-4">
-      <div class="col-md-8 col-12">
-        <CodeContainer :title="'Concept'">
-          <template v-slot:sourceCode>
-            <pre>
-// the compiler will enforce one abstract method (s-a-m)
-@FunctionalInterface
-public interface Printable {
-    String print(String suffix);
-}
-        </pre
-            >
-          </template>
-        </CodeContainer>
-      </div>
-      <div class="col-md-8 col-12">
-        <CodeContainer :title="''">
-          <template v-slot:sourceCode>
-            <pre>
-public class Lambdas {
-    public static void main(String[] args) {
-        Printable printable = s -> "Meow" + s;
-    }
-}
-        </pre
-            >
-          </template>
-        </CodeContainer>
-      </div>
-      <div class="col-md-8 col-12">
-        <CodeContainer :title="'Example'">
-          <template v-slot:sourceCode>
-            <pre>
-public interface Printable {
-    void print();
-}
-        </pre
-            >
-          </template>
-        </CodeContainer>
-      </div>
-      <div class="col-md-8 col-12">
-        <CodeContainer :title="''">
-          <template v-slot:sourceCode>
-            <pre>
-public class Lambdas {
-    public static void main(String[] args) {
-        Cat cat = new Cat(); // Cat implements Printable
-        printThing(cat);
+  <div class="row m-4">
+    <div class="col-md-8 col-12">
+      <CodeContainer :title="'Single Parameter'">
+        <template v-slot:sourceCode>
+          <pre>
+Consumer&lt;String&gt; consumer = (String str) -> System.out.println(str);
+consumer.accept("test");
 
-        Printable printable = () -> System.out.println("test");
-        printThing(printable);
+Function&lt;String, Integer&gt; function = (String str) -> Integer.parseInt(str);
+Integer apply = function.apply("123");
 
-        printThing(() -> {
-            System.out.println(1);
-            System.out.println(2);
-        });
-    }
-    
-    static void printThing(Printable thing) {
-        thing.print();
-    }    
-}
-        </pre
-            >
-          </template>
-        </CodeContainer>
-      </div>
+Predicate&lt;String&gt; predicate = (String str) -> str.equals("test");
+boolean check = predicate.test("abc");
+
+Supplier&lt;Integer&gt; supplier = () -> new Random().nextInt(3);
+Integer integer = supplier.get();
+          </pre>
+        </template>
+      </CodeContainer>
+    </div>
+    <div class="col-md-8 col-12">
+      <CodeContainer :title="'Two Parameter'">
+        <template v-slot:sourceCode>
+          <pre>
+BiConsumer&lt;Person, byte[]&gt; biConsumer = (person, arr) -> {
+    person.setHeight(arr[0]);
+    person.setWeight(arr[1]);
+};
+biConsumer.accept(person, new byte[]{200, 100});
+          </pre>
+        </template>
+      </CodeContainer>
     </div>
   </div>
 </template>
